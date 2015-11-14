4791e366f3afb91be6dff18dcd6ecce7475ec7a1
Al Bueche IV <albert.bueche.iv@gmail.com>
2015-11-13 23:19:35 -0800

    removed other project artifacts, IN PROGRESS: toggle expansion

--- rpg.js
+++ rpg.js
@@ -1,85 +1,122 @@
 var recursiveApp = angular.module('rpgApp', []);
 
-recursiveApp.controller('rpgCtrl', function($scope) {
-	$scope.test="test";
-	
-	$scope.data = [{  
-		"BatchID":35,
-		"Product":{
-			"Description":"Thingamabob",
-			"ProductID":12,
-			"Components":[  
-				{
-					"Requirement":"Sprocket",
-					"Quantity":1,
-					"FulfilledBy":[
-						{  
-							"BatchID":87,
-							"Product":{  
-								"Description":"Sprocket",
-								"ProductID":3,
-								"Components":{  
-								}
-							}						
+recursiveApp.controller('rpgCtrl', function($scope, $rootScope) {
+	$scope.warrior = {
+		'actions': {
+			'name': 'Actions',
+			'id': 'actionGroup',
+			'list': [
+				{	'name': 'Combo',
+					'id': 'comboGroup',
+					'list': [
+						{
+							'name': 'Rush',
+							'id': 'combo_rush',		
+							'description': 'Knockdown -> Power Strike',
+						},
+						{
+							'name': '3-hit',
+							'id': 'combo_3hit',
+							'description': 'Jab -> Jab -> Power Strike',
 						}
 					]
 				},
-				{
-					"Requirement":"Gizmo",
-					"Quantity":5,
-					"FulfilledBy":[ 
-						{  
-							"BatchID":32,
-							"Product":{  
-								"Description":"Gizmo",
-								"ProductID":54,
-								"Components":{  
-								}
-							}
+				{	'name': 'Ability',
+					'id': 'abilityGroup',
+					'list': [
+						{
+							'name': 'Challenge',
+							'id': 'ability_challenge',
+							'description': 'Encourage an enemy to fight only you (limit: 1 active)',
 						},
-						{  
-							"BatchID":33,
-							"Product":{  
-								"Description":"Gizmo",
-								"ProductID":54,
-								"Components":{  
-
-								}
-							}
+						{
+							'name': 'Sweep',
+							'id': 'ability_sweep',
+							'description': 'Knockdown every nearby enemy',
+						},
+						{
+							'name': 'Throw Axe',
+							'id': 'ability_throwingAxe',
+							'description': 'Hurl a throwing axe',
+						}
+					]
+				},
+				{	'name': 'Attack',
+					'id': 'attackGroup',
+					'list': [
+						{
+							'name': 'Jab',
+							'id': 'attack_jab',
+							'description': 'Quick strike',
+						},
+						{
+							'name': 'Power Strike',
+							'id': 'attack_powerStrike',
+							'description': 'Slower, more powerful attack',
+						},
+						{
+							'name': 'Knockdown',
+							'id': 'attack_knockdown',
+							'description': 'Strong shove designed to knock the enemy prone',
 						}
 					]
 				}
 			]
-		}		
-	}];
+		}
+	};
 	
-	$scope.toggle = function(field) {
+	$scope.getAction = function(id, actionGroup) {
+		if (id == actionGroup.id) {
+			console.log(id,'==',actionGroup.id);
+			return actionGroup;
+		} else {
+			console.log(id,'!=',actionGroup.id);
+			for (var action in actionGroup) {
+				var theAction = $scope.getAction(id, action);
+				if (theAction) {
+					return theAction;
+				}
+			}
+		}
+		return null;
+	};
+	
+	$scope.getWarriorAction = function(id) {
+		return $scope.getAction(id, $scope.warrior.actions); 
+	};
+	
+	$scope.toggleExpansion = function(id) {
+		console.log('toggleExpansion',id);
+		var field = $scope.getWarriorAction(id);
 		field.expanded = !field.expanded;
+	};
+	
+	$scope.$on("toggleExpansion", function(event, args) {
+		console.log("received toggleExpansion", args);
+		var field = $scope.getWarriorAction(args);
+		field.expanded = !field.expanded;
+	});
+	
+	$scope.sendToggleExpansion = function(id) {
+		$rootScope.$broadcast("toggleExpansion", id);
 	};
 });
 
-recursiveApp.directive('batch', function() {
+recursiveApp.directive('actionGroup', function() {
 	return { 
 		restrict : 'E',
 		scope : { 
-			batch: '=*data',
+			data: '=*data',
+			sendToggleExpansion: '&'
 		},
-		template : '<div ng-include="\'batch.html\'"></div>'
+		template : '<div ng-include="\'actionGroup.html\'"></div>'
 	};
-}).directive('product', function() {
+}).directive('action', function() {
 	return {
 		restrict : 'E',
 		scope : { 
-			product: '=*data',
+			data: '=*data',
 		},
-		template : '<div ng-include="\'product.html\'"></div>'
+		template : '<div ng-include="\'action.html\'"></div>'
 	};
-}).directive('component', function() {
-	return {
-		restrict : 'E',
-		scope : { 
-			component: '=*data',
-		},
-		template : '<div ng-include="\'component.html\'"></div>'
-	};
-})
+});
