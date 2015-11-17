1ea4892df1f3fba2c4376c668fe1e0d1ca723d6e
Al Bueche IV <albert.bueche.iv@gmail.com>
2015-11-16 19:59:17 -0800

    action durations, current character based on queued actions

--- rpg.js
+++ rpg.js
@@ -34,16 +34,19 @@ recursiveApp.controller('rpgCtrl', function($scope, $rootScope) {
 						{
 							'name': 'Challenge',
 							'id': 'ability_challenge',
+							'duration': 10,
 							'description': 'Encourage an enemy to fight only you (limit: 1 active)',
 						},
 						{
 							'name': 'Sweep',
 							'id': 'ability_sweep',
+							'duration': 40,
 							'description': 'Knockdown every nearby enemy',
 						},
 						{
 							'name': 'Throw Axe',
 							'id': 'ability_throwingAxe',
+							'duration': 30,
 							'description': 'Hurl a throwing axe',
 						}
 					]
@@ -55,22 +58,26 @@ recursiveApp.controller('rpgCtrl', function($scope, $rootScope) {
 						{
 							'name': 'Jab',
 							'id': 'attack_jab',
+							'duration': 20,
 							'description': 'Quick strike',
 						},
 						{
 							'name': 'Power Strike',
 							'id': 'attack_powerStrike',
+							'duration': 40,
 							'description': 'Slower, more powerful attack',
 						},
 						{
 							'name': 'Knockdown',
 							'id': 'attack_knockdown',
+							'duration': 20,
 							'description': 'Strong shove designed to knock the enemy prone',
 						}
 					]
 				}
 			]
-		}
+		},
+		'durationQueued': 0
 	};
 	$scope.archer = {
 		'name': 'Archer',
@@ -103,16 +110,19 @@ recursiveApp.controller('rpgCtrl', function($scope, $rootScope) {
 						{
 							'name': 'Snare',
 							'id': 'ability_snare',
-							'description': 'Create a immobile trap',
+							'duration': 80,
+							'description': 'Create an immobile trap (limit: 1 active)',
 						},
 						{
 							'name': 'Barrage',
 							'id': 'ability_barrage',
+							'duration': 40,
 							'description': 'Release a lot of inaccurate arrows - "Quantity over quality"',
 						},
 						{
 							'name': 'Ignite',
 							'id': 'ability_ignite',
+							'duration': 40,
 							'description': 'Fire a flaming arrow',
 						}
 					]
@@ -124,22 +134,26 @@ recursiveApp.controller('rpgCtrl', function($scope, $rootScope) {
 						{
 							'name': 'Plunk',
 							'id': 'attack_plunk',
+							'duration': 20,
 							'description': 'Quick shot',
 						},
 						{
 							'name': 'Power Shot',
 							'id': 'attack_powerShot',
+							'duration': 40,
 							'description': 'Slower, more powerful attack',
 						},
 						{
 							'name': 'Pin',
 							'id': 'attack_pin',
+							'duration': 40,
 							'description': 'Aim for the legs, designed to lock the enemy in place',
 						}
 					]
 				}
 			]
-		}
+		},
+		'durationQueued': 0
 	};
 	
 	$scope.addCharacter = function(aChar) {
@@ -180,15 +194,21 @@ recursiveApp.controller('rpgCtrl', function($scope, $rootScope) {
 		else {
 			if (action.combo) {
 				for (i in action.combo) {
-					$scope.getActiveCharacter().actionQueue.push($scope.getAction(action.combo[i]));
+					$scope.queueAction($scope.getActiveCharacter(), $scope.getAction(action.combo[i]));
 				}
 			}
 			else {
-				$scope.getActiveCharacter().actionQueue.push(action);
+				$scope.queueAction($scope.getActiveCharacter(), action);
 			}
 			$scope.nextCharacter();
 		}
 		$scope.updateactionDisplay();
+	};
+	
+	$scope.queueAction = function(character, action) {
+		action.durationLeft = action.duration;
+		character.actionQueue.push(action);
+		character.durationQueued += action.duration;
 	};
 	
 	$scope.actionMenu_back = function() {
@@ -197,9 +217,10 @@ recursiveApp.controller('rpgCtrl', function($scope, $rootScope) {
 	};
 	
 	$scope.nextCharacter= function() {
-		$scope.activeCharacterIndex++;
-		if ($scope.activeCharacterIndex >= $scope.characters.length) {
-			$scope.activeCharacterIndex = 0;
+		for (var i in $scope.characters) {
+			if ($scope.characters[i].durationQueued < $scope.characters[$scope.activeCharacterIndex].durationQueued) {
+				$scope.activeCharacterIndex = i;
+			}
 		}
 	};
 	
